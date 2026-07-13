1602e0ed5068acc82f6a8f9efd9784ece934120c
ahito35p0813-code <ahito35p0813@gmail.com>
2026-07-13 15:39:00 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -61,11 +61,8 @@ function startGame(level){
     createPlayer("cpu4","CPU4",20);
 
     document.getElementById("log").innerHTML =
-
         "<b>ERASER CLASH</b><br><br>" +
-
         "ゲーム開始！<br>" +
-
         "CPUレベル：" + level;
 
 }
@@ -76,41 +73,36 @@ function startGame(level){
 
 function createPlayer(id,name,hp){
 
-    document.getElementById(id).innerHTML =
-
-    `
+    document.getElementById(id).innerHTML = `
         <strong>${name}</strong>
-
         <br>
-
         ❤️ ${hp}/20
-
         <div class="hpBar">
-
             <div class="hp"></div>
-
         </div>
     `;
 
 }
 /*==============================
-初期データ
+ゲームデータ
 ==============================*/
 
 const game = {
 
-    turn:1,
+    level : "",
 
-    deck:[],
+    turn : 1,
 
-    trash:[],
+    deck : [],
 
-    players:[]
+    trash : [],
+
+    players : []
 
 };
 
 /*==============================
-ログ
+ログ追加
 ==============================*/
 
 function addLog(text){
@@ -124,34 +116,11 @@ function addLog(text){
 }
 
 /*==============================
-今後追加する処理
+初期化
 ==============================*/
 
-// v0.2
-// 山札生成
+window.addEventListener("load",()=>{
 
-// v0.3
-// 初期手札2枚
+    console.log("ERASER CLASH v0.1.0");
 
-// v0.4
-// ターン開始
-
-// v0.5
-// 攻カード
-
-// v0.6
-// 防カード
-
-// v0.7
-// 能カード
-
-// v0.8
-// 場カード
-
-// v0.9
-// CPU思考
-
-// v1.0
-// 勝敗判定
-
-console.log("ERASER CLASH v0.1.0 Ready");
+});
