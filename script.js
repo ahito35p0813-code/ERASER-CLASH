5a6ffd3d3db2630ab7711ee050ea7cd0c5f43b76
ahito35p0813-code <ahito35p0813@gmail.com>
2026-07-19 16:10:57 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
-// タイトル画面
 const titleScreen = document.getElementById("title-screen");
-
-// バトル画面
 const battleScreen = document.getElementById("battle-screen");
 
-// タップしたらメニューへ
+// 最初はバトル画面を隠す
+battleScreen.style.display = "none";
+
+// タイトルを押したらメニュー
 titleScreen.addEventListener("click", () => {
+
     titleScreen.innerHTML = `
         <h1>MENU</h1>
 
-        <button class="menu-btn">CPUバトル</button>
+        <button id="cpuBtn" class="menu-btn">CPUバトル</button>
         <button class="menu-btn">ルールブック</button>
         <button class="menu-btn">カード図鑑</button>
         <button class="menu-btn">消しゴム図鑑</button>
         <button class="menu-btn">NEWS</button>
         <button class="menu-btn">設定</button>
     `;
-    const cpuButton = document.querySelector(".menu-btn");
 
-cpuButton.onclick = () => {
-    titleScreen.style.display = "none";
-    battleScreen.style.display = "block";
-};
+    document.getElementById("cpuBtn").onclick = () => {
+        titleScreen.style.display = "none";
+        battleScreen.style.display = "block";
+    };
+
 });
